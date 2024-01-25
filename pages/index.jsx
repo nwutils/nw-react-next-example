098b5f48efd7fac96a9a6dc86f632728ab0cc653
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2024-01-24 22:50:29 -0500

    chore: setup basic linting

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,16 @@
+import React from "react";
+
 function App() {
     return (
         <div>
-            NW Next.js Example
+            NW.js Next.js Example
+            <br />
+            <ul>
+                <li>NW.js: {nw.require('process').version['nw']}</li>
+                <li>Chromium: {nw.require('process').version['chromium']}</li>
+                <li>Node.js: {nw.require('process').version['node']}</li>
+                <li>V8: {nw.require('process').version['v8']}</li>
+            </ul>
         </div>
     );
 };
