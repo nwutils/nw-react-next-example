e231dc4fff2492ed0d86aadce0a0a062309b8a85
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2024-04-20 21:46:49 +0530

    chore: update

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,15 +1,31 @@
+"use client"
+
 import React from "react";
 
+/**
+ * React hook to use NW.js object.
+ * 
+ * @returns {object | undefined} NW.js object
+ */
+function useNW() {
+    if (typeof window !== "undefined" && typeof window.nw !== "undefined") {
+        return window.nw;
+    } else {
+        return undefined;
+    }
+}
+
 function App() {
+    const nw = useNW();
     return (
         <div>
             NW.js Next.js Example
             <br />
             <ul>
-                <li>NW.js: {nw.require('process').version['nw']}</li>
-                <li>Chromium: {nw.require('process').version['chromium']}</li>
-                <li>Node.js: {nw.require('process').version['node']}</li>
-                <li>V8: {nw.require('process').version['v8']}</li>
+                <li>NW.js: {nw && nw.require('process').version['nw']}</li>
+                <li>Chromium: {nw && nw.require('process').version['chromium']}</li>
+                <li>Node.js: {nw && nw.require('process').version['node']}</li>
+                <li>V8: {nw && nw.require('process').version['v8']}</li>
             </ul>
         </div>
     );
---
+++ server.js
@@ -0,0 +1,12 @@
+import { exec } from 'child_process';
+
+const command = 'next dev -p 4000';
+
+exec(command, (error, stdout, stderr) => {
+    if (error) {
+        console.error(`exec error: ${error}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.error(`stderr: ${stderr}`);
+});
