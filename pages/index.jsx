c015ca903ea1e1b590d81c5e6f06d96f157cce60
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2025-02-03 23:25:58 +0530

    fix: get dev version working (#75)

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,15 +1,30 @@
 import React from "react";
 
+function useNW () {
+    const [nw, setNW] = React.useState(null);
+
+    React.useEffect(() => {
+        if (window.require) {
+            setNW(window.nw);
+        }
+    }, []);
+
+    return nw;
+}
+
 function App() {
+
+    const nw = useNW();
+
     return (
         <div>
             NW.js Next.js Example
             <br />
             <ul>
-                <li>NW.js: {nw.require('process').version['nw']}</li>
-                <li>Chromium: {nw.require('process').version['chromium']}</li>
-                <li>Node.js: {nw.require('process').version['node']}</li>
-                <li>V8: {nw.require('process').version['v8']}</li>
+                <li>NW.js: {nw?.require('process')?.version['nw']}</li>
+                <li>Chromium: {nw?.require('process')?.version['chromium']}</li>
+                <li>Node.js: {nw?.require('process')?.version['node']}</li>
+                <li>V8: {nw?.require('process')?.version['v8']}</li>
             </ul>
         </div>
     );
